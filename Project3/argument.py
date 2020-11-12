3add3a57e46b28c1e1eddc162c81273e0b94d49e
panargirakis <panargirakis@gmail.com>
2020-11-12 01:06:02 -0500

    removed comments, reduced replay memory size

--- Project3/argument.py
+++ Project3/argument.py
@@ -12,7 +12,7 @@ def add_arguments(parser):
     parser.add_argument('--batch_size', type=int, default=32, help='batch size for training')
     parser.add_argument('--log_interval', type=int, default=100, help='time steps between log entries')
     parser.add_argument('--learning_rate', type=float, default=0.0001, help='learning rate for training')
-    parser.add_argument('--memory_size', type=int, default=1500, help='size of replay memory')
+    parser.add_argument('--memory_size', type=int, default=1300, help='size of replay memory')
     parser.add_argument('--continue_training', action='store_true', default=False,
                         help='Whether to continue training from an existing model')
     parser.add_argument('--target_update_int', type=int, default=10000, help='the target update interval in steps')
