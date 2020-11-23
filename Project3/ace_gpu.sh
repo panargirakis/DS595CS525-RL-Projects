a97fbf5d5f377cc54ea94b4640fdd2c91555b7f7
panargirakis <panargirakis@gmail.com>
2020-11-22 23:24:40 -0500

    add different ace configurations

--- Project3/ace_gpu.sh
+++ Project3/ace_gpu.sh
@@ -2,11 +2,17 @@
 #SBATCH -N 1
 #SBATCH -n 12
 #SBATCH --mem 16000
-#SBATCH --gres=gpu:0
+#SBATCH --gres=gpu:1
+#SBATCH -C K20
 #SBATCH -o log.txt
 
 # Load any needed environment modules
 module load python/gcc-8.2.0/3.7.2
+module load cuda92/blas/9.2.88
+module load cuda92/fft/9.2.88
+module load cuda92/nsight/9.2.88
+module load cuda92/profiler/9.2.88
+module load cuda92/toolkit/9.2.88
 
 # Set python virtual env
 source /home/pargyrakis/DS595CS525-RL-Projects/venv/bin/activate
@@ -14,4 +20,5 @@ source /home/pargyrakis/DS595CS525-RL-Projects/venv/bin/activate
 # Execute my commands
 cd /home/pargyrakis/DS595CS525-RL-Projects/Project3
 pip install -r requirement.txt
+pip install torch==1.7.0+cu92 torchvision==0.8.1+cu92 torchaudio==0.7.0 -f https://download.pytorch.org/whl/torch_stable.html
 python main.py --train_dqn > train_log.txt
